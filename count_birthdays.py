e1cf86d633f18d6423fb5be6010fe0fa0353eb0e
Temerold <tobbe@temerold.se>
2023-06-12 22:38:24 +0200

    Update `count_birthdays.py`

--- count_birthdays.py
+++ count_birthdays.py
@@ -36,11 +36,29 @@ def count_birthdays(
     In this example, the function `count_birthdays()` is used to count the
     number of birthdays that fall on October 15 within the range from October
     15, 2008, to December 31, 2023. The output is "15", indicating that there
-    are "14" occurrences of birthdays on October 15 within the specified range.
+    are 15 occurrences of birthdays on October 15 within the specified range.
     """
     count = 0
     while start_date <= end_date:
+        # We add a day to `start_date` to avoid counting the first birthday in
+        # one's life (not counting as a year)
+        start_date += timedelta(days=1)
         if start_date.month == month and start_date.day == day:
             count += 1
-        start_date += timedelta(days=1)
-    return count - 1
+
+    return count
+
+
+if __name__ == "__main__":
+    with open("README.md", "r") as file:
+        lines = file.readlines()
+    with open("README.md", "a") as file:
+        file.truncate(0)
+        first_line = lines[0]
+        file.write(first_line)
+        file.write(
+            f"## Hi. I'm a {count_birthdays()}-year-old programmer from Sweden"
+            ":)\n"
+        )
+        for line in lines[2:-1]:
+            file.write(line)
